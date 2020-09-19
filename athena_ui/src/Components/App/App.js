3e38246d6431feda80b78bc742893215ad9c4c01
NTramsen <neiltramsen@gmail.com>
2020-09-19 18:53:57 -0400

    Created main user dashboard

---
+++ athena_ui/src/Components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Checkout from '../Checkout/Checkout';
+
+const App = () => {
+	return (
+		<div className = 'main-container'>
+			<div className = 'top-banner'>
+			<h>Welcome User6969</h>
+			</div>
+			<Checkout/>
+		</div>
+	);
+
+}
+
+export default App;
---
+++ athena_ui/src/Components/Checkout/Checkout.js
@@ -0,0 +1,49 @@
+import React, {useContext, useState} from 'react';
+import './style.css';
+
+const Checkout = ()=>{
+
+	const [newItemName, setNewItemName] = useState('');
+	const [newItemNum, setNewItemNum] = useState('');
+	
+	const updateItems = ()=>{
+		console.log(newItemName, newItemNum);
+	};
+
+	const itemObjectValid = ()=>{
+		const numValid = newItemNum && Number.parseFloat(newItemNum);
+		const nameValid = newItemName && newItemName.split('').find(char=>char !== ' ');
+		return numValid && nameValid;
+	};
+
+	const clearForm =()=>{
+		setNewItemName('');
+		setNewItemNum('');
+	};
+
+
+	return(
+		<div className='checkout-container'>
+			<input className = 'checkout-form'
+				placeholder = 'Enter item name'
+				type = 'text'
+				value = {newItemName}
+				onChange={(e)=>setNewItemName(e.target.value)}></input>
+			<input className = 'checkout-form'
+				placeholder = 'Enter item number'
+				type = 'number'
+				value = {newItemNum}
+				onChange={(e)=>setNewItemNum(e.target.value)}></input>
+			<button className = 'checkout-button btn'
+				onClick={()=>{
+					if(itemObjectValid()){
+						// updateBills
+						updateItems();
+						clearForm();
+					}
+				}}>Add item</button>
+		</div>
+	);
+};
+
+export default Checkout;
