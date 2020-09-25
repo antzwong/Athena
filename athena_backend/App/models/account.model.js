d5cce4c41ef5239f8af1328cdfc16468f0f74731
OTLandU <65378290+OTLandU@users.noreply.github.com>
2020-09-25 21:55:21 +0800

    Add files via upload

---
+++ athena_backend/App/models/account.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const accountSchema = new Schema(
+    {
+        username: 
+        {
+            type: String,
+            required: true,
+        },
+        password:
+        {
+            type: String,
+            required: true,
+        }
+    }
+);
---
+++ athena_backend/App/models/actionlog.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const actionlogSchema = new Schema(
+    {
+        // which user borrowed the equipemnts
+        username: 
+        {
+            type: String,
+            required: true,
+        },
+        // which equipment borrowed
+        // should store the object id of the equipment
+        equipment:
+        {
+            type: String, 
+            required: true,
+        },
+        // should provide what date the equipment should be returned
+        returndate:
+        {
+            type: Date,
+            required: true,
+        },
+        // the status of rather or not the equipment had been returned
+        borrowedstatus:
+        {
+            type: Boolean, default:'false'
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
